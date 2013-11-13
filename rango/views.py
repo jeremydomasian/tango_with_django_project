4027a391369bbbd0bd82132b42e0dd1825c0d4e0
Jeremy Domasian <spiffyxd@gmail.com>
2013-11-12 20:13:40 -0800

    4.1.1 Configuring the Templates Directory

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello world!")
+	return HttpResponse(
+		'Rango says hello world! <a href="/rango/about">Check out the about page?</a>'
+		)
+
+def about(request):
+	return HttpResponse("Rango Says: Here is the about page. <a href='/rango/'>Go back home?</a>")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_PATH = os.getcwd()
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+    )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
